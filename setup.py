4f38946db3e2af9e66303c3c47b632a99008a302
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2023-04-10 16:29:36 +0200

    updating setup file 2

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setuptools.setup(
     long_description="Just testin packaging for sum function",
     long_description_content_type="text/markdown",
     url='https://github.com/Tripper98/SUM_FUNCTION',
-    packages=['src\sumtripper'],
+    packages=['sumtripper'],
     install_requires=['h5py', 'typing-extensions', 'wheel'],
 )
