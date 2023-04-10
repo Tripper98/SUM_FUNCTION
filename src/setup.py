ab3cd2b9be8e436228112fdb660d700d05198145
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2023-04-10 16:10:11 +0200

    adding setup file

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='sumtripper',
+    version='0.0.1',
+    author='A.D Tripper',
+    author_email='adnanedriouche98@gmail.com',
+    description='Testing installation of Package',
+    long_description="Just testin packaging for sum function",
+    long_description_content_type="text/markdown",
+    url='https://github.com/Tripper98/SUM_FUNCTION',
+    packages=['sumtripper'],
+    install_requires=['requests'],
+)
