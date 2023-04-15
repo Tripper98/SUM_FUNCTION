0a086d65d61c55300617c5ce8d5f75ada9aada7e
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2023-04-15 14:44:31 +0200

    feat: adding sum list function

--- sumtripper/ad_sum.py
+++ sumtripper/ad_sum.py
@@ -1,2 +1,8 @@
 def ad_sum(a, b):
     return a+b
+
+def sum_list(*numbers):
+    res = 0 
+    for el in numbers:
+        res += el
+    return el
--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,6 +1,11 @@
-from sumtripper.ad_sum import ad_sum
+from sumtripper.ad_sum import ad_sum, sum_list
 
 
 def test_ad_sum():
     x = ad_sum(5,5)
     assert x == 10, "Should be 10"
+
+
+def test_sum_list():
+    x = sum_list(5,5,5)
+    assert x == 15, "Should be 15"
